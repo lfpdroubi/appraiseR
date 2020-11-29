96c23f68d6c69f0b8d9bb3468136753483218c73
Luiz Fernando Palin Droubi <lfpdroubi@gmail.com>
2020-11-29 16:16:03 +0000

    split plotVar function into plotFactor and plotContinuousVariable for better maintainability

--- R/plotVar.R
+++ R/plotVar.R
@@ -18,6 +18,13 @@
 #' library(sf)
 #' data(centro_2015)
 #' dados <- st_drop_geometry(centro_2015)
+#' mod1 <- lm(sqrt(valor) ~ sqrt(area_total) + suites, dados)
+#' plotVar(mod1, "area_total")
+#' plotVar(mod1, "area_total", func = "sqrt")
+#' plotVar(mod1, "suites")
+#' plotVar(mod1, "suites", func = "sqrt")
+#' plotVar(mod1, "area_total", interval = "confidence", ca = TRUE)
+#' plotVar(mod1, "area_total", interval = "confidence", func = "sqrt", ca = TRUE)
 #' mod <- lm(log(valor) ~ ., data = dados)
 #' plotVar(mod, "area_total")
 #' plotVar(mod, "area_total", interval = "confidence")
@@ -34,7 +41,7 @@
 #'         dist_b_mar = 250, padrao = "medio"))
 #' plotVar(mod, "area_total", func = "log", interval = "both", ca = TRUE,
 #'         local = list(area_total = 205, quartos = 3, suites = 1, garagens = 2,
-#'         dist_b_mar = 250, padrao = "medio"))
+#'         dist_b_mar = 250, padrao = "medio"), av = 1100000)
 #' plotVar(mod, "padrao")
 #' plotVar(mod, "padrao", ca = TRUE)
 #' plotVar(mod, "padrao", func = "log", ca = TRUE)
@@ -74,162 +81,218 @@ plotVar <- function(object, variable, func,
   variavel <- df[, variable, drop = FALSE]
 
   if (is.factor(variavel[, variable, drop = T])){
-    grid <- unique(variavel)
-    if (missing(local)) {
-      new <- data.frame(grid, lapply(df[setdiff(preds, variable)], centre))
-      p_local <- NULL
-    } else {
-      new <- data.frame(grid, local[setdiff(preds, variable)])
-      p_local <- predict(z, newdata = local)
-    }
-    if(interval != "both") {
-      Y <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
-                             interval = interval, level = level, ...)
-    } else {
-      message("Plotagem de ambos os intervalos disponivel apenas para variaveis
-              continuas. Adotado intervalo de predicao")
-      Y <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
-                             interval = "prediction", level = level, ...)
-    }
-    if (!missing(func)) {
-      Y <- inverse(Y, func)
-      Y <- cbind(Y, campo_arbitrio(Y))
-    }
-    pred <- data.frame(new[ , variable, drop = FALSE], Y)
-    pred_plot <- reshape2::melt(pred,
-                                id.var = variable,
-                                value.name = response,
-                                variable.name = "var")
-    p <- ggplot(data = pred_plot, aes(x = reorder(!!as.name(variable),
-                                                  !!as.name(response),
-                                                  median),
-                                       y = !!as.name(response))) +
-      geom_boxplot(aes_(fill = as.name(variable))) +
-      xlab(variable) +
-      theme(legend.position="none") +
-      scale_y_continuous(labels = scales::label_number_si(accuracy = .01,
-                                                          big.mark = ".",
-                                                          decimal.mark = ","))
-    if(!missing(local)) {
-      p_local <- ifelse(missing(func), p_local, inverse(p_local, func))
-      cat(elasticidade(z, variable, func, local, factor = +1), " ")
-      if (!missing(av)) {
-        p_local <- data.frame(y = p_local, local, av = av)
-      } else {
-        p_local <- data.frame(y = p_local, local)
-      }
-      names(p_local)[1] <- response
-      p <- p + geom_point(data = p_local,
-                          aes_(x = as.name(variable),
-                               y = as.name(response)),
-                          color = "red",
-                          size = 2)
-    }
-    if (ca == TRUE & missing(func)) {
-        message("Campo de Arbítrio somente possivel na escala original.")
-    }
-    if (!missing(av) & missing(local)){
-      message("Valor arbitrado somente pode ser plotado caso seja informado local.")
-    } else if (!missing(av)) {
-      p <- p + geom_point(data = p_local,
-                          aes_(x = as.name(variable), y = ~av),
-                          color = "purple",
-                          size = 2)
-    }
-    return(p)
+    plotFactor(variable, z, interval = interval, level = level, func = func,
+               ca = ca, av = av, local = local, ...)
   } else {
-    # caso variavel continua
-    grid <- seq(min(df[, variable], na.rm = TRUE),
-                max(df[, variable], na.rm = TRUE),
-                length = 101)
-    if (missing(local)) {
-      new <- data.frame(grid, lapply(df[setdiff(preds, variable)], centre))
-      p_local <- NULL
+    plotContinuousVariable(variable, z, interval = interval, level = level,
+                           func = func, ca = ca, av = av, local = local, ...)
+  }
+}
+#' export
+plotFactor <- function(x, object,
+                       interval =  c("none", "confidence", "prediction", "both"),
+                       level = 0.80,
+                       local,
+                       func,
+                       ca = FALSE,
+                       av,
+                       ...){
+  interval <- match.arg(interval)
+  variable <- x
+  z <- object
+  params <- parameters(z)
+  response <- params$response
+  preds <- params$predictors
+
+  df <- eval(stats::getCall(z)$data)
+  variavel <- df[, x, drop = FALSE]
+  grid <- unique(variavel)
+
+  if (missing(local)) {
+    new <- data.frame(grid, lapply(df[setdiff(preds, variable)], centre))
+    p_local <- NULL
+  } else {
+    new <- data.frame(grid, local[setdiff(preds, variable)])
+    p_local <- predict(z, newdata = local)
+  }
+  if(interval != "both") {
+    Y <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
+                           interval = interval, level = level, ...)
+  } else {
+    message("Plotagem de ambos os intervalos disponivel apenas para variaveis
+              continuas. Adotado intervalo de predicao")
+    Y <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
+                           interval = "prediction", level = level, ...)
+  }
+  if (!missing(func)) {
+    Y <- inverse(Y, func)
+    Y <- cbind(Y, campo_arbitrio(Y))
+  }
+  pred <- data.frame(new[ , variable, drop = FALSE], Y)
+  pred_plot <- reshape2::melt(pred,
+                              id.var = variable,
+                              value.name = response,
+                              variable.name = "var")
+  p <- ggplot(data = pred_plot, aes(x = reorder(!!as.name(variable),
+                                                !!as.name(response),
+                                                median),
+                                    y = !!as.name(response))) +
+    geom_boxplot(aes_(fill = as.name(variable))) +
+    xlab(variable) +
+    theme(legend.position="none") +
+    scale_y_continuous(labels = scales::label_number_si(accuracy = .01,
+                                                        big.mark = ".",
+                                                        decimal.mark = ","))
+  if(!missing(local)) {
+    p_local <- ifelse(missing(func), p_local, inverse(p_local, func))
+    cat(elasticidade(z, variable, func, local, factor = +1), " ")
+    if (!missing(av)) {
+      p_local <- data.frame(y = p_local, local, av = av)
     } else {
-      new <- data.frame(grid, local)
-      p_local <- predict(z, newdata = local)
+      p_local <- data.frame(y = p_local, local)
     }
-    names(new)[1] <- variable
-    if(interval != "both") {
-      Y <- stats::predict.lm(object = z, newdata = new, interval = interval,
+    names(p_local)[1] <- response
+    p <- p + geom_point(data = p_local,
+                        aes_(x = as.name(variable),
+                             y = as.name(response)),
+                        color = "red",
+                        size = 2)
+  }
+  if (ca == TRUE & missing(func)) {
+    message("Campo de Arbítrio somente possivel na escala original.")
+  }
+  if (!missing(av) & missing(local)){
+    message("Valor arbitrado somente pode ser plotado caso seja informado local.")
+  } else if (!missing(av)) {
+    p <- p + geom_point(data = p_local,
+                        aes_(x = as.name(variable), y = ~av),
+                        color = "purple",
+                        size = 2)
+  }
+  return(p)
+}
+#'
+#' @export
+plotContinuousVariable <- function(x, object,
+                                   interval =  c("none", "confidence", "prediction", "both"),
+                                   level = 0.80,
+                                   local,
+                                   func,
+                                   ca = FALSE,
+                                   av,
+                                   ...){
+  interval <- match.arg(interval)
+  variable <- x
+  z <- object
+  params <- parameters(z)
+  response <- params$response
+  preds <- params$predictors
+
+  df <- eval(stats::getCall(z)$data)
+  variavel <- df[, x, drop = FALSE]
+
+  grid <- seq(min(df[, variable], na.rm = TRUE),
+              max(df[, variable], na.rm = TRUE),
+              length = 101)
+
+  if (missing(local)) {
+    new <- data.frame(grid, lapply(df[setdiff(preds, variable)], centre))
+    p_local <- NULL
+  } else {
+    new <- data.frame(grid, local)
+    p_local <- predict(z, newdata = local)
+  }
+
+  names(new)[1] <- variable
+
+  if(interval != "both") {
+    Y <- stats::predict.lm(object = z, newdata = new, interval = interval,
                            level = level, ...)
-    } else {
-      Y1 <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
-                              interval = "confidence", level = level, ...)
-      Y2 <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
-                              interval = "prediction", level = level, ...)
-      Y1 <- as.data.frame(Y1)
-      Y2 <- as.data.frame(Y2)
-      Y <- dplyr::inner_join(Y1, Y2, by = "fit")
-    }
-    if (!missing(func)) {
-      Y <- inverse(Y, func)
-      Y <- cbind(Y, campo_arbitrio(Y))
-    }
-    pred <- data.frame(grid, Y)
-    colnames(pred)[1] <- variable
-    colnames(pred)[2] <- response
-    p <- ggplot(data = pred, aes_(x = as.name(variable), y = as.name(response))) +
-        geom_line(size = 1) +
-        theme(legend.position="bottom") +
-        scale_y_continuous(labels = scales::label_number_si(accuracy = .01,
-                                                            big.mark = ".",
-                                                            decimal.mark = ",")) +
-        scale_x_continuous(labels = scales::label_number_si(accuracy = .01,
-                                                            big.mark = ".",
-                                                            decimal.mark = ",")) +
-      theme(axis.text.x=element_text(angle = 45, vjust = 1, hjust = 1))
-    if (ca == TRUE) {
-      if (missing(func)) {
-        message("Campo de Arbítrio somente possivel na escala original.")
-        return(p)
-      } else {
-        p <- p +
-          geom_line(aes_string(y = "C.A.I."), linetype = 2) +
-          geom_line(aes_string(y = "C.A.S."), linetype = 2)
-      }
-    }
-    if(!missing(local)) {
-      p_local <- ifelse(missing(func), p_local, inverse(p_local, func))
-      cat(elasticidade(z, variable, func, local), " ")
-      if (!missing(av)) {
-        p_local <- data.frame(y = p_local, local, av = av)
-      } else {
-        p_local <- data.frame(y = p_local, local)
-      }
-      names(p_local)[1] <- response
-      p <- p + geom_point(data = p_local,
-                          aes_(x = as.name(variable),
-                               y = as.name(response)),
-                          color = "red",
-                          size = 2)
-    }
-    if (!missing(av) & missing(local)){
-      message("Valor arbitrado somente pode ser plotado caso seja informado local.")
-    } else if (!missing(av)) {
-      p <- p + geom_point(data = p_local,
-                          aes_(x = as.name(variable), y = ~av),
-                          color = "purple",
-                          size = 2)
-    }
-    if (interval == "none") {
-      return(p)
-    } else if (interval != "both"){
-      p <- p + geom_ribbon(aes_(ymin = ~lwr, ymax = ~upr,
-                                colour = "grey", alpha = 0.5),
-                           stat = "identity") +
-        theme(legend.position="none")
-      return(p)
+  } else {
+    Y1 <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
+                            interval = "confidence", level = level, ...)
+    Y2 <- stats::predict.lm(object = z, newdata = remove_missing_levels(z, new),
+                            interval = "prediction", level = level, ...)
+    Y1 <- as.data.frame(Y1)
+    Y2 <- as.data.frame(Y2)
+    Y <- dplyr::inner_join(Y1, Y2, by = "fit")
+  }
+
+  if (!missing(func)) {
+    Y <- inverse(Y, func)
+    Y <- cbind(Y, campo_arbitrio(Y))
+  }
+
+  pred <- data.frame(grid, Y)
+  colnames(pred)[1] <- variable
+  colnames(pred)[2] <- response
+
+  # Basic Plot
+  p <- ggplot(data = pred, aes_(x = as.name(variable), y = as.name(response))) +
+    geom_line(size = 1) +
+    theme(legend.position="bottom") +
+    scale_y_continuous(labels = scales::label_number_si(accuracy = .01,
+                                                        big.mark = ".",
+                                                        decimal.mark = ",")) +
+    scale_x_continuous(labels = scales::label_number_si(accuracy = .01,
+                                                        big.mark = ".",
+                                                        decimal.mark = ",")) +
+    theme(axis.text.x=element_text(angle = 45, vjust = 1, hjust = 1))
+
+
+  # Adds CA, if TRUE
+  if (ca == TRUE) {
+    if (missing(func)) {
+      message("Campo de Arbítrio somente possivel na escala original.")
     } else {
       p <- p +
-        geom_ribbon(aes_(ymin = ~lwr.y, ymax = ~upr.y,
-                         colour = "grey", alpha = 0.25),
-                    stat = "identity") +
-        geom_ribbon(aes_(ymin = ~lwr.x, ymax = ~upr.x,
-                         colour = "grey", alpha = .25),
-                    stat = "identity") +
-        theme(legend.position="none")
-      return(p)
+        geom_line(aes_string(y = "C.A.I."), linetype = 2) +
+        geom_line(aes_string(y = "C.A.S."), linetype = 2)
     }
+  }
+
+  # Adds chosen intervals ribbons, if any
+  if (interval == "both") {
+    p <- p +
+      geom_ribbon(aes_(ymin = ~lwr.y, ymax = ~upr.y,
+                       colour = "grey", alpha = 0.25),
+                  stat = "identity") +
+      geom_ribbon(aes_(ymin = ~lwr.x, ymax = ~upr.x,
+                       colour = "grey", alpha = .25),
+                  stat = "identity") +
+      theme(legend.position="none")
+  } else if (interval != "none"){
+    p <- p + geom_ribbon(aes_(ymin = ~lwr, ymax = ~upr,
+                              colour = "grey", alpha = 0.5),
+                         stat = "identity") +
+      theme(legend.position="none")
+  }
+
+  # Adds point of local argument, if not missing
+  if(!missing(local)) {
+    p_local <- ifelse(missing(func), p_local, inverse(p_local, func))
+    cat(elasticidade(z, variable, func, local), " ")
+    if (!missing(av)) {
+      p_local <- data.frame(y = p_local, local, av = av)
+    } else {
+      p_local <- data.frame(y = p_local, local)
     }
+    names(p_local)[1] <- response
+    p <- p + geom_point(data = p_local,
+                        aes_(x = as.name(variable),
+                             y = as.name(response)),
+                        color = "red",
+                        size = 2)
+  }
+
+  # Adds arbitrated value, if not missing
+  if (!missing(av) & missing(local)){
+    message("Valor arbitrado somente pode ser plotado caso seja informado local.")
+  } else if (!missing(av)) {
+    p <- p + geom_point(data = p_local,
+                        aes_(x = as.name(variable), y = ~av),
+                        color = "purple",
+                        size = 2)
+  }
+  return(p)
 }
