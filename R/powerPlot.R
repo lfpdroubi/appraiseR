2f942d1357942029cf50eb56f781bcea2cb1929d
LUIZ FERNANDO PALIN DROUBI <lfpdroubi@gmail.com>
2021-01-07 19:18:16 -0300

    adds default method for powerPlot.

--- R/powerPlot.R
+++ R/powerPlot.R
@@ -3,46 +3,100 @@
 #'
 #' \code{powerPlot} generates Power Plots for \code{\link{lm}} or
 #' \code{\link{bestfit}} objects with \code{\link{ggplot2}}.
-#'
-#' @param object object of class \code{\link{lm}} or \code{\link{bestfit}}
-#' @param func function used to transform the dependent variable
-#' @param axis option to plot predicted values on the x axis (inverted) or in
-#' the y axis (standard)
-#' @param smooth option to add a regression line to the plot
-#' @param \dots not used.
+#' @param y observed values
+#' @param yhat predicted values
 #' @return a power plot
 #' @name powerPlot
 #' @export
-powerPlot <- function(object, ...) {
+powerPlot <- function(y, yhat, ...) {
   UseMethod("powerPlot")
 }
 
-#' @rdname powerPlot
+#' @name powerPlot
+#' @param axis option to plot predicted values on the x axis (inverted) or in
+#' the y axis (standard)
+#' @param smooth option to add a regression line to the plot
 #' @examples
 #' library(sf)
 #' dados <- st_drop_geometry(centro_2015)
 #' dados$padrao <- as.numeric(dados$padrao)
 #' fit <- lm(log(valor) ~ area_total + quartos + suites + garagens +
 #' log(dist_b_mar) + I(1/padrao), dados, subset = -c(31, 39))
+#' s <- summary(fit)
+#' # Mediana
+#' library(ggplot2)
+#' p <- powerPlot(y = na.omit(dados$valor)[-c(31,39)], yhat = exp(fitted(fit)),
+#' axis = "inverted")
+#' p + labs(title = "Poder de Predição", subtitle = "Mediana")
+#' # Média
+#' p1 <- powerPlot(y = na.omit(dados$valor)[-c(31,39)],
+#'                 yhat = exp(fitted(fit) + s$sigma^2/2),  axis = "inverted")
+#' p1 + labs(title = "Poder de Predição", subtitle = "Média")
+#' # Moda
+#' p2 <- powerPlot(y = na.omit(dados$valor)[-c(31,39)],
+#'                 yhat = exp(fitted(fit) - s$sigma^2), axis = "inverted")
+#' p2 + labs(title = "Poder de Predição", subtitle = "Moda")
+#' @export
+powerPlot.default <- function(y, yhat, axis = c("standard", "inverted"),
+                              smooth = TRUE, se = FALSE, ...){
+  axis <- match.arg(axis)
+  invres <- data.frame(y = y, yhat = yhat)
+
+  if (axis == "inverted") {
+    p <- ggplot(invres, aes(x = yhat, y = y)) +
+      geom_point(alpha=0.5) +
+      xlab(bquote(~hat(Y))) +
+      ylab("Y") +
+      geom_abline(color = "red")
+  } else {
+    p <- ggplot(invres, aes(x = y, y = yhat)) +
+      geom_point(alpha=0.5) +
+      ylab(bquote(~hat(Y))) +
+      xlab("Y") +
+      geom_abline(color = "red")
+  }
+
+  if (smooth == TRUE) {
+    p <- p + stat_smooth(method = "lm", se = se)
+  }
+
+  RMSE <- paste("RMSE = ", brf(Metrics::rmse(y, yhat), nsmall = 0))
+  MAE <- paste("MAE = ", brf(Metrics::mae(y, yhat), nsmall = 0))
+  MAPE <- paste("MAPE =", pct(Metrics::mape(y, yhat), digits = 2))
+
+  p <- p +
+    ggpmisc::geom_label_npc(aes(npcx = "left", npcy = "top", label = RMSE),
+                            color = "blue") +
+    ggpmisc::geom_label_npc(aes(npcx = "right", npcy = "bottom", label = MAE),
+                            color = "green") +
+    ggpmisc::geom_label_npc(aes(npcx = "center", npcy = "bottom", label = MAPE),
+                            color = "red")
+  return(p)
+
+}
+
+#' @rdname powerPlot
+#' @param object object of class \code{\link{lm}} or \code{\link{bestfit}}
+#' @param func function used to transform the dependent variable
+#' @param \dots further args passed to powerPlot.default
+#' @examples
 #' powerPlot(fit)
 #' powerPlot(fit, se = TRUE)
 #' powerPlot(fit, smooth = FALSE)
 #' powerPlot(fit, axis = "inverted")
-#' library(ggplot2)
 #' p <- powerPlot(fit, func = "log", axis = "inverted")
 #' p + labs(title = "Poder de Predição", subtitle = "Em milhões de Reais")
+#' # Changes Dep Var Transformation
 #' fit <- lm(sqrt(valor) ~ area_total + quartos + suites + garagens +
 #' log(dist_b_mar) + I(1/padrao), dados)
 #' powerPlot(fit)
 #' powerPlot(fit, func = "sqrt", axis = "inverted")
 #' @export
 #'
-powerPlot.lm <- function(object, func, axis = c("standard", "inverted"),
-                         smooth = TRUE, se = FALSE, ...) {
+powerPlot.lm <- function(object, func, ...) {
   z <- object
   attr(z$terms, "variables")
   data <- stats::model.frame(z)
-  axis <- match.arg(axis)
 
   Y <- data[, attr(z$terms, "response")]
   Y_ajustado <- z$fitted.values
@@ -52,21 +106,7 @@ powerPlot.lm <- function(object, func, axis = c("standard", "inverted"),
     Y_ajustado <- inverse(Y_ajustado, func)
   }
 
-  invres <- data.frame(Y, Y_ajustado)
-
-  if (axis == "inverted") {
-    p <- ggplot(data = invres, aes(x = Y_ajustado, y = Y)) +
-      geom_point(alpha=0.5) +
-      xlab(bquote(~hat(Y))) +
-      ylab("Y") +
-      geom_abline(color="red")
-  } else {
-    p <- ggplot(data = invres, aes(x = Y, y = Y_ajustado)) +
-      geom_point(alpha=0.5) +
-      ylab(bquote(~hat(Y))) +
-      xlab("Y") +
-      geom_abline(color="red")
-  }
+  p <- powerPlot(Y, Y_ajustado, ...)
 
   if (!missing(func)) {
     p <- p +
@@ -76,10 +116,6 @@ powerPlot.lm <- function(object, func, axis = c("standard", "inverted"),
       scale_x_continuous(labels = scales::label_number_si(accuracy = .01,
                                                           big.mark = ".",
                                                           decimal.mark = ","))
-  }
-
-  if (smooth == TRUE) {
-    p <- p + stat_smooth(method = "lm", se = se)
   }
 
   return(p)
