d677fd6a21c3cd716c89071b9f8e25d06cd980cf
Luiz F P Droubi <lfpdroubi@gmail.com>
2024-02-19 10:12:24 -0300

    adds system argumento do powerPlot

--- R/powerPlot.R
+++ R/powerPlot.R
@@ -15,7 +15,7 @@ powerPlot <- function(y, yhat, ...) {
 #' @name powerPlot
 #' @param axis option to plot predicted values on the x axis (inverted) or in
 #' the y axis (standard)
-#' @param smooth option to add a regression line to the plot
+#' @param Smooth option to add a regression line to the plot
 #' @param se option to add confidence interval of regression line to the plot
 #' @param metrics TRUE or FALSE. If TRUE, metrics (RMSE, MAE and MAPE) are
 #' displayed at the plot area.
@@ -23,69 +23,92 @@ powerPlot <- function(y, yhat, ...) {
 #' are printed.
 #' @examples
 #' library(sf)
-#' dados <- st_drop_geometry(centro_2015[1:50, ])
-#' dados$padrao <- as.numeric(dados$padrao)
-#' fit <- lm(log(valor) ~ area_total + quartos + suites + garagens +
-#' log(dist_b_mar) + I(1/padrao), dados, subset = -c(31, 39))
-#' s <- summary(fit)
-#' # Mediana
+#' library(broom)
 #' library(ggplot2)
-#' y <- dados$valor[-c(31, 39)]
-#' yhat <- exp(fitted(fit))
-#' p <- powerPlot(y, yhat, axis = "inverted")
+#' centro_2015$padrao <- as.numeric(centro_2015$padrao)
+#' fit <- lm(log(valor) ~ area_total + quartos + suites + garagens +
+#'           log(dist_b_mar) + I(1/padrao),
+#'           data = centro_2015, subset = -c(31, 39))
+#' s <- summary(fit)
+#' centro_2015 <- expand.model.frame(fit, ~ valor + dist_b_mar + padrao)
+#' # Transformed Scale:
+#' y <- centro_2015$`log(valor)`[complete.cases(centro_2015)]
+#' powerPlot(y = y, yhat = fitted(fit),
+#'           system = "ggplot2", Smooth = TRUE)
+#' powerPlot(y = y, yhat = fitted(fit),
+#'           system = "ggplot2", Smooth = TRUE, axis = "inverted")
+#' # Original Scale:
+#' ## Mediana
+#' Y <- centro_2015$valor[complete.cases(centro_2015)]
+#' YHAT <-  exp(fitted(fit))
+#' p <- powerPlot(y = Y,
+#'                yhat = YHAT,
+#'                system = 'ggplot2',
+#'                axis = "inverted",
+#'                Smooth = TRUE)
 #' p + labs(title = "Poder de Predição", subtitle = "Mediana")
-#' # Média
-#' yhat <- exp(fitted(fit) + s$sigma^2/2)
-#' p1 <- powerPlot(y = y, yhat = yhat , axis = "inverted")
+#' ## Média
+#' YHAT1 <- exp(fitted(fit) + s$sigma^2/2)
+#' p1 <- powerPlot(y = Y,
+#'                 yhat = YHAT1,
+#'                 system = 'ggplot2',
+#'                 axis = "inverted",
+#'                 Smooth = TRUE)
 #' p1 + labs(title = "Poder de Predição", subtitle = "Média")
-#' # Moda
-#' yhat <- exp(fitted(fit) - s$sigma^2)
-#' p2 <- powerPlot(y = y, yhat = yhat, axis = "inverted")
+#' ## Moda
+#' YHAT2 <- exp(fitted(fit) - s$sigma^2)
+#' p2 <- powerPlot(y = Y,
+#'                 yhat = YHAT2,
+#'                 system = 'ggplot2',
+#'                 axis = "inverted",
+#'                 Smooth = TRUE)
 #' p2 + labs(title = "Poder de Predição", subtitle = "Moda")
 #' @export
-powerPlot.default <- function(y, yhat, axis = c("standard", "inverted"),
-                              smooth = TRUE, methods = "lm", se = FALSE,
-                              metrics = c("rmse", "mae"), R2 = TRUE, ...){
+powerPlot.default <- function(y, yhat, system = c("base", "ggplot2", "lattice"),
+                              axis = c("standard", "inverted"),
+                              Smooth = FALSE, methods = "lm", se = FALSE,
+                              metrics = c("none", "rmse", "mae", "mape"),
+                              R2 = FALSE, ...){
   axis <- match.arg(axis)
-  invres <- data.frame(y = y, yhat = yhat)
+  system <- match.arg(system)
+  metrics <- match.arg(metrics, several.ok = TRUE)
 
-  if (axis == "inverted") {
-    p <- ggplot(invres, aes(x = yhat, y = y)) +
-      geom_point(alpha=0.5) +
-      xlab(bquote(~hat(Y))) +
-      ylab("Y") +
-      geom_abline(color = "red")
-  } else {
-    p <- ggplot(invres, aes(x = y, y = yhat)) +
-      geom_point(alpha=0.5) +
-      ylab(bquote(~hat(Y))) +
-      xlab("Y") +
-      geom_abline(color = "red")
-  }
-
-  if (smooth == TRUE) {
-    i <- 2
-    for (method in methods) {
-      i <- i + 1
-      p <- p + stat_smooth(method = method, se = se, color = i)
-    }
-  }
-
-  if (R2 == TRUE){
-    RSQ <- substitute(R^2~"="~r2,
-                      list(r2 = brf(cor(y, yhat)^2, nsmall = 2)))
-    p <- p +
-      ggpp::geom_label_npc(aes(npcx = "center", npcy = "top",
-                                  label = as.character(as.expression(RSQ))),
-                              parse = TRUE,
-                              color = "darkblue")
-  }
-
-  if (isFALSE(metrics)) {
-    return(p)
+  if (system == "ggplot2") {
+    invres <- data.frame(y = y, yhat = yhat)
+    if (axis == "inverted") {
+      p <- ggplot(invres, aes(x = yhat, y = y)) +
+        geom_point(alpha=0.5) +
+        xlab(bquote(~hat(Y))) +
+        ylab("Y") +
+        geom_abline(color = "red")
     } else {
-      if (isTRUE(metrics)) metrics <- c("rmse", "mae", "mape")
+      p <- ggplot(invres, aes(x = y, y = yhat)) +
+        geom_point(alpha=0.5) +
+        ylab(bquote(~hat(Y))) +
+        xlab("Y") +
+        geom_abline(color = "red")
+    }
+    if (Smooth == TRUE) {
+      i <- 2
+      for (method in methods) {
+        i <- i + 1
+        p <- p + stat_smooth(method = method, se = se, color = i)
+      }
+    }
 
+    if (R2 == TRUE){
+      RSQ <- substitute(R^2~"="~r2,
+                        list(r2 = brf(cor(y, yhat)^2, nsmall = 2)))
+      p <- p +
+        ggpp::geom_label_npc(aes(npcx = "center", npcy = "top",
+                                 label = as.character(as.expression(RSQ))),
+                             parse = TRUE,
+                             color = "darkblue")
+    }
+
+    if ("none" %in% metrics) {
+      return(p)
+    } else {
       npcx <- c(rmse = "left", mae = "right", mape = "center")
       npcy <- c(rmse = "top", mae = "bottom", mape = "bottom")
       col <- c(rmse = "blue", mae = "darkgreen", mape = "red")
@@ -93,18 +116,64 @@ powerPlot.default <- function(y, yhat, axis = c("standard", "inverted"),
       a <- vector(mode = "character", length = 0)
       for (metric in metrics) {
         a[[metric]] <- paste(metric, " = ",
-                   brf(do.call(metric, args = list(y, yhat)),
-                       nsmall = 0))
+                             brf(do.call(metric, args = list(y, yhat)),
+                                 nsmall = 0))
       }
       df <- data.frame(x.char = npcx[metrics], y.char =  npcy[metrics],
                        text = a[metrics], color = col[metrics])
       p <- p +
         ggpp::geom_label_npc(data = df,
-                                aes(npcx = x.char, npcy = y.char, label = text),
-                                color = col[metrics]
-                                )
+                             aes(npcx = x.char, npcy = y.char, label = text),
+                             color = col[metrics]
+        )
     }
-  return(p)
+    return(p)
+  } else if (system == "lattice") {
+    f <- as.formula(paste("y ~ yhat"))
+    xyplot(f,
+           panel = function(x, y) {
+             panel.xyplot(x, y)
+             panel.abline(lm(y ~ x))
+    }) +
+    latticeExtra::layer(panel.smoother(y ~ x, se = FALSE, span = 0.5))
+  } else {
+    if (axis == "standard") {
+      car::scatterplot(x = y, y = yhat,
+                       col = "grey",
+                       boxplots = "",
+                       id = T,
+                       pch = 20,
+                       cex = .75,
+                       las = 1,
+                       regLine = list(method=lm, lty=1, lwd=2, col="green"),
+                       smooth=list(smoother = loessLine, style = "none",
+                                   lty.smooth = 1, col.smooth = "blue"),
+                       xlab = "Y",
+                       ylab = bquote(~hat(Y)),
+                       ...
+      )
+      abline(a = 0, b = 1, col = "red")
+    } else {
+      car::scatterplot(x = yhat, y = y,
+                       col = "grey",
+                       boxplots = "",
+                       id = T,
+                       pch = 20,
+                       cex = .75,
+                       las = 1,
+                       pty = "s",
+                       regLine = list(method=lm, lty=1, lwd=2, col="green"),
+                       smooth=list(smoother = loessLine, style = "none",
+                                   lty.smooth = 1, col.smooth = "blue"),
+                       xlab = bquote(~hat(Y)),
+                       ylab = "Y",
+                       ...
+      )
+      abline(a = 0, b = 1, col = "red")
+    }
+
+  }
+
 }
 
 #' @rdname powerPlot
@@ -114,64 +183,94 @@ powerPlot.default <- function(y, yhat, axis = c("standard", "inverted"),
 #' of this function will be used to retransform the data to the original scale).
 #' @param \dots further args passed to powerPlot.default
 #' @examples
+#' # powerPlot.lm:
+#' par(pty="s") #default par(pty="m")
 #' powerPlot(fit)
-#' powerPlot(fit, se = TRUE)
-#' powerPlot(fit, smooth = FALSE)
+#' powerPlot(fit, main = "Poder de Predição")
+#' powerPlot(fit, func = "log",
+#'           main = "Poder de Predição")
+#' title(sub = "Em reais", adj = 1)
 #' powerPlot(fit, axis = "inverted")
-#' p <- powerPlot(fit, scale = "original", func = "log", axis = "inverted")
-#' p + labs(title = "Poder de Predição", subtitle = "Em milhões de Reais")
+#' p <- powerPlot(fit, system = "ggplot2",
+#'                func = "log", axis = "inverted")
+#' p +
+#'   labs(title = "Poder de Predição", subtitle = "Em Reais") +
+#'   theme(aspect.ratio = 1)
 #' # Changes Dep Var Transformation
-#' fit <- lm(sqrt(valor) ~ area_total + quartos + suites + garagens +
-#' log(dist_b_mar) + I(1/padrao), dados)
-#' powerPlot(fit)
-#' powerPlot(fit, func = "sqrt", axis = "inverted")
+#' fit1 <- lm(sqrt(valor) ~ area_total + quartos + suites + garagens +
+#'           log(dist_b_mar) + I(1/padrao), centro_2015)
+#' powerPlot(fit1)
+#' powerPlot(fit1, func = "sqrt", axis = "inverted")
 #' @export
 #'
-powerPlot.lm <- function(object, func, ...) {
+powerPlot.lm <- function(object, func, system = c("base", "ggplot2", "lattice"),
+                         ...) {
   z <- object
   data <- stats::model.frame(z)
+  system <- match.arg(system)
 
-  Y <- data[, attr(z$terms, "response")]
-  Y_ajustado <- z$fitted.values
+  params <- parameters(z)
+  depvar <- params$response
+  lhs <- params$lhs
+
+  df <- expand.model.frame(model = z, extras = depvar, na.expand = TRUE)
+  df$`.fitted` <- fitted(z)
+  #df <- broom::augment(z, data = df)
 
   if (!missing(func)) {
-    Y <- inverse(Y, func)
-    Y_ajustado <- inverse(Y_ajustado, func)
+    df[, ".Fitted"] <- inverse(df[, ".fitted"], func)
+    y <- df[, depvar]
+    yhat <- df[, ".Fitted"]
+  } else {
+    y <- df[, lhs]
+    yhat <- df[, ".fitted"]
   }
 
-  p <- powerPlot(Y, Y_ajustado, ...)
-
-  if (!missing(func)) {
-    p <- p +
-      scale_y_continuous(labels = scales::label_number(accuracy = .01,
-                                                       big.mark = ".",
-                                                       decimal.mark = ",")) +
-      scale_x_continuous(labels = scales::label_number(accuracy = .01,
-                                                       big.mark = ".",
-                                                       decimal.mark = ","))
+  if (system == "ggplot2") {
+    if (!missing(func)) {
+      p <- powerPlot(y, yhat, system = system, ...)
+      p <- p +
+        scale_y_continuous(labels = scales::label_number(accuracy = .01,
+                                                         big.mark = ".",
+                                                         decimal.mark = ",")) +
+        scale_x_continuous(labels = scales::label_number(accuracy = .01,
+                                                         big.mark = ".",
+                                                         decimal.mark = ","))
+      return(p)
+    } else {
+      p <- powerPlot(y, yhat, system = system, ...)
+      return(p)
+    }
+  } else if (system == "base") {
+    powerPlot(y, yhat, system = system, ...)
+    #abline(a = 0, b = 1, col = "red")
   }
-
-  return(p)
 }
 
 #' @rdname powerPlot
 #' @param fit chosen fit
 #' @param scale Plot values in original or transformed scale?
 #' @examples
+#' # powerPlot.bestfit:
+#' library(sf)
 #' dados <- st_drop_geometry(centro_2015)
-#' dados <- within(dados, padrao <- as.numeric(padrao))
+#' dados$padrao <- as.numeric(dados$padrao)
 #' best_fit <- bestfit(valor ~ ., dados,
 #'                     transf = c("rec", "rsqrt", "log", "sqrt"),
 #'                     subset = -c(31, 39))
-#' p1 <- powerPlot(best_fit)
-#' p2 <- powerPlot(best_fit, scale = "original")
+#' p1 <- powerPlot(best_fit, system ="ggplot2")
+#' p2 <- powerPlot(best_fit, system ="ggplot2", scale = "original")
 #' cowplot::plot_grid(p1, p2, nrow = 1, ncol = 2)
-#' p1 <- powerPlot(best_fit, axis = "inverted")
-#' p2 <- powerPlot(best_fit, axis = "inverted", scale = "original")
+#' p1 <- powerPlot(best_fit, system = "ggplot2",
+#'                 axis = "inverted")
+#' p2 <- powerPlot(best_fit, system = "ggplot2",
+#'                 axis = "inverted", scale = "original")
 #' cowplot::plot_grid(p1, p2, nrow = 1, ncol = 2)
 #' powerPlot(best_fit, fit = 514)
-#' p1 <- powerPlot(best_fit, fit = 514, axis = "inverted")
-#' p2 <- powerPlot(best_fit, fit = 514, scale = "original", axis = "inverted")
+#' p1 <- powerPlot(best_fit, fit = 514, system = "ggplot2",
+#'                 axis = "inverted")
+#' p2 <- powerPlot(best_fit, fit = 514, system = "ggplot2",
+#'                 scale = "original", axis = "inverted")
 #' cowplot::plot_grid(p1, p2, nrow = 1, ncol = 2)
 #'
 #' @export
@@ -193,6 +292,7 @@ powerPlot.bestfit <- function(object, fit = 1,
 
 #' @rdname powerPlot
 #' @examples
+#' # powerPlot.lmerMod:
 #' library(lme4)
 #' Mfit <- lmer(log(valor) ~ area_total + quartos + suites + garagens +
 #' dist_b_mar + (1|padrao), dados)
